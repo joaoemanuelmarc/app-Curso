e8b9925a706a0c3682d083b8b719e02708bf958c
João E. Marcelino <joaoemanuelmarc@gmail.com>
2023-10-27 22:52:11 -0300

    Aula 27/10 mobile
    Criação das funções manipula e correções de bugs

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -8,31 +8,46 @@ use App\Models\Categoria;
 
 class CategoriaController extends Controller
 {
-    //abri o formulario de cadastro
-    public function mostrarFormCat(){
-        return view('cad_categoria');
+        //abri o formulario de cadastro
+        public function mostrarFormCat(){
+            return view('cad_categoria');
+        }
+
+        public function index(){
+            return view('index');
+        }
+
+
+    public function cadastroCat(Request $request){
+        //verifica se exiate algo na variavel nomecategoria.
+    $registrosCat = $request->validate([
+        'nomecategoria'=> 'string|required'
+    ]);
+
+    //esta linha é que grava o registro no banco.
+    Categoria::create($registrosCat);
+
+    return Redirect::route('index');
+
+    }
+    public function mostrarManipulaCategoria(){
+        $registrosCategoria = Categoria::All();
+
+        return view('manipula_categoria',['registrosCategoria' => $registrosCategoria]);
     }
 
-    public function index(){
-        return view('index');
+    public function DeletarCategoria(Categoria $registrosCategoria){
+        $registrosCategoria->delete();
+        return Redirect::route('index');       
     }
 
-public function cadastroCat(Request $request){
-    //verifica se exiate algo na variavel nomecategoria.
-$registrosCat = $request->validate([
-    'nomecategoria'=> 'string|required'
-]);
-
-//esta linha é que grava o registro no banco.
- Categoria::create($registrosCat);
-
- return Redirect::route('index');
-
-}
-public function mostrarManipulaCategoria(){
-    $registrosCategoria = Categoria::All();
-
-    return view('manipula_categoria',['registrosCategoria' => $registrosCategoria]);
-}
+    public function BuscarCategoriaNome(Request $request){
+        $registrosCategoria = Categoria::query();
+        $registrosCategoria->when($request->nomecategoria,function($query, $valor){
+            $query->where('nomecategoria','like','%',$valor,'%');
+        });
+        $registrosCategoria = $registrosCategoria->get();
+        return  view('manipula_categoria',['registrosCategoria' => $registrosCategoria]);
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AulaController;
 use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\AppController;
+
 
 Route::get('/',[CategoriaController::class,'index'])->name('index');
 
@@ -15,13 +17,24 @@ Route::get('/cadcurso',[CursoController::class,'mostrarFormCurso'])->name("form-
 //rota para cadastrar categoria
 Route::post('/cadcurso',[CursoController::class,'cadastroCurso'])->name("cadastro-curso");
 
+Route::post('/cadcurso',[CursoController::class,'cadastroCurso'])->name("manipula-curso");
+
+
 Route::get('/cadaula',[AulaController::class,'mostrarFormAula'])->name("form-cadastro-aula");
 //rota para cadastrar categoria
 Route::post('/cadaula',[AulaController::class,'cadastroAula'])->name("cadastro-aula");
 //rotas para manipular categoria
-Route::get('/manipulacategoria',[CategoriaController::class,'mostrarManipulaCategoria'])->name("manipula-categoria");
+Route::get('/manipula_categoria',[CategoriaController::class,'mostrarManipulaCategoria'])->name("manipula-categoria");
 
-//rotas para alterar categoria
+//rota para buscar por nome da categoria
+Route::get('/manipulanomecategoria',[CategoriaController::class,'BuscarCategoriaNome'])->name("buscar-categoria-nome");
+
+//rota para deletar a categoria
+Route::delete('/deletarcategoria/{registrosCategoria}',[CategoriaController::class,'DeletarCategoria'])->name('deletar-categoria');
+
+
+//rotas para mostrar as categoria
 Route::get('/alterar-categoria/{registrosCategoria}',[CategoriaController::class,'MostrarAlterarCategoria'])->name('alterar-categoria');
+Route::put('/alterarbancocategoria/{registrosCategoria}',[CategoriaController::class,'AlterarBancoCategoria'])->name('alterar-banco-categoria');
 
-
+Route::get('/manipula-categoria',[CategoriaController::class,'mostrarManipulaCategoria'])->name("manipula-aula");
